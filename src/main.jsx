cb5be4c357a0dde435f16f838109b4a7fc1c839b
Luke Wagner <you@example.com>
2025-07-25 10:18:55 -0400

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    // By removing <React.StrictMode>, we prevent the development-only double-render
+    // that is causing the input focus to be lost. This will not affect the production build.
+    <App />
 );
